d19d66344ddb89d74490ae4a393cbd3f451dda9c
Yoganathan.R <94101011+21IT63@users.noreply.github.com>
2023-11-18 11:27:22 +0530

    Add files via upload

---
+++ Average.java
@@ -0,0 +1,30 @@
+Scanner scanner = new Scanner(System.in);
+
+System.out.print("Enter the number of elements: ");
+int n = scanner.nextInt();
+
+// Check if n is greater than 0
+if (n <= 0) {
+    System.out.println("Please enter a valid number of elements greater than 0.");
+    return;
+}
+
+double sum = 0;
+
+System.out.println("Enter the elements:");
+
+// Input n numbers
+for (int i = 0; i < n; i++) {
+    System.out.print("Element " + (i + 1) + ": ");
+    double num = scanner.nextDouble();
+    sum += num;
+}
+
+// Calculate the average
+double average = sum / n;
+
+// Display the result
+System.out.println("The average is: " + average);
+
+// Close the scanner
+scanner.close();
